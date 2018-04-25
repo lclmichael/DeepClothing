358cc3e9d23a7dae94dfdb548592a2266031f156
lclmichael <lclluffy@163.com>
2018-04-26 00:10:51 +0800

    add var 3

--- model/multiclass_network.py
+++ model/multiclass_network.py
@@ -21,25 +21,26 @@ class MultiClassNetwork(object):
         self.stddev = stddev
         self.lighten_vgg = LightenVGG()
 
-    def dense_layer(self, input_tensor, output_size):
+    def dense_layer(self, input_tensor, output_size, use_bias=True):
         fc = tf.layers.dense(inputs=tf.layers.flatten(input_tensor),
                               units=output_size,
                               kernel_initializer=tf.initializers.truncated_normal(stddev=self.stddev),
-                              use_bias=False)
-        bn = tf.layers.batch_normalization(fc, training=self.is_train)
-        return bn
+                              use_bias=use_bias)
+        # bn = tf.layers.batch_normalization(fc, training=self.is_train)
+        return fc
 
     def build_model(self):
         pool5 = self.lighten_vgg.get_model(self.input_x, self.is_train, self.stddev)
         #直接接入全连接层输出
-        fc1 = self.dense_layer(pool5, 4096)
-        fc2 = self.dense_layer(fc1, 4096)
-        logits = self.dense_layer(fc2, self.output_size)
+        # fc1 = self.dense_layer(pool5, 4096)
+        # fc2 = self.dense_layer(fc1, 4096)
+        logits = self.dense_layer(pool5, self.output_size)
 
         y_prediction = tf.nn.softmax(logits)
         comparison = tf.equal(tf.argmax(y_prediction, 1), tf.argmax(self.y_truth, 1))
         accuracy = tf.reduce_mean(tf.cast(comparison, dtype=tf.float32))
-        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y_truth, logits=logits))
+        loss = tf.losses.softmax_cross_entropy(onehot_labels=self.y_truth, logits=logits)
+        # loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y_truth, logits=logits))
         extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
         with tf.control_dependencies(extra_update_ops):
             train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(loss)
