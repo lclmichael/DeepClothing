1d7b92f7dca59de4bb894493aa66f581b65f4f3d
lclmichael <lclluffy@163.com>
2018-05-16 17:59:28 +0800

    test 16

---
+++ data/caltech101/get_mean.py
@@ -0,0 +1,19 @@
+# encoding=utf8
+# Author=LclMichael
+
+import tensorflow as tf
+
+from deepclothing.data.caltech101.json_data import json_data_tools
+
+import deepclothing.util.image_utils as image_utils
+
+def get_mean(file = "all.json"):
+    datas = json_data_tools.get_data_list(file)
+    print(image_utils.get_images_mean(datas[0]))
+
+def main():
+    get_mean()
+    pass
+    
+if __name__ == "__main__":
+    main()
